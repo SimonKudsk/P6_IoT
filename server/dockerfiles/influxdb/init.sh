008662602cf03005c5ca505459f0bf240b51ecd2
Jakob <bokaj_19@hotmail.com>
2025-04-30 22:34:50 +0200

    Update Docker configuration and MQTT functionality

--- server/dockerfiles/influxdb/init.sh
+++ server/dockerfiles/influxdb/init.sh
@@ -1,16 +1,31 @@
 #!/bin/bash
+
+# Indlæs .env-variabler
+set -a
+source .env
+set +a
+
 # Wait until InfluxDB is ready
 sleep 5
 
-# Opret ekstra bucket til test
+# Opret ekstra bucket til nodered
 influx bucket create \
-  --name testBucket \
-  --org IoT-Org \
-  --token SuperSecretToken \
-  --host http://localhost:8086
+  --name nodered \
+  --org "$INFLUXDB_ORG" \
+  --token "$INFLUXDB_ADMIN_TOKEN"  \
+  --host "http://${INFLUXDB_HOSTNAME}:${INFLUXDB_PORT}"
 
-echo "testBucket created!"
+echo "nodered testBucket created!"
 
+
+# Opret python hardware bucket til drift
+influx bucket create \
+  --name IoT-Bucket \
+  --org "$INFLUXDB_ORG" \
+  --token "$INFLUXDB_ADMIN_TOKEN"  \
+  --host "http://${INFLUXDB_HOSTNAME}:${INFLUXDB_PORT}"
+
+echo "python hardware testBucket created!"
 
 
 
