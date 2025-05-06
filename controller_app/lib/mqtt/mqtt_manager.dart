0062966a46565941dd03c4f2315640b1b203ce59
J-Vigen <johanne-vigen@hotmail.com>
2025-05-06 11:03:45 +0200

    Browserclient in MQTT for testing
    Connection working

--- controller_app/lib/mqtt/mqtt_manager.dart
+++ controller_app/lib/mqtt/mqtt_manager.dart
@@ -1,46 +1,120 @@
-import 'package:flutter/foundation.dart';
+
 import 'package:mqtt_client/mqtt_client.dart';
 import 'package:mqtt_client/mqtt_server_client.dart';
 import 'package:mqtt_client/mqtt_browser_client.dart';
-
+/* Server connection
+//https://www.emqx.com/en/blog/using-mqtt-in-flutter
 class MqttClientWrapper {
+// The ? signifies that the variable client is nullable – meaning it is allowed to hold the value null
   MqttClient? client;
 
-  MqttClientWrapper(String brokerAddress, int brokerPort, String clientId, {required String websocketUrl}) {
-    client = MqttBrowserClient(websocketUrl, clientId);
-    if (kIsWeb) {
-      // Running on the web, use MqttBrowserClient with the websocket URL
-      print('Initializing MqttBrowserClient for web...');
-      client = MqttBrowserClient(websocketUrl, clientId);
-    } else {
-      // Running on native platforms (mobile, tablet), use MqttServerClient
-      print('Initializing MqttServerClient for native...');
-      client = MqttServerClient.withPort(brokerAddress, clientId, brokerPort);
-    }
+
+  MqttClientWrapper(String brokerAddress, int brokerPort, String clientId) {
+    print('Initializing MqttServerClient...');
+    client = MqttServerClient.withPort(brokerAddress, clientId, brokerPort);
 
     client?.logging(on: true);
     client?.keepAlivePeriod = 120;
 
   final connMessage = MqttConnectMessage()
+  //authentication here if needed
   .withWillTopic('willTopic')
   .withWillMessage('willMessage')
   .startClean()
   .withWillQos(MqttQos.atLeastOnce);
 
-  client?.connectionMessage = connMessage;
-}
+
+*/
+class MqttClientWrapper {
+  MqttClient? client;
+  final String identifier;
+
+  MqttClientWrapper(String brokerUrl, this.identifier) {
+    print('Initializing MqttBrowserClient for $identifier...');
+    client = MqttBrowserClient(brokerUrl, identifier);
+
+    client?.port = 8084;
+    client?.logging(on: true);
+    client?.keepAlivePeriod = 60;
+
+
+    //Callbacks
+    client?.onConnected = onConnected;
+    client?.onDisconnected = onDisconnected;
+    client?.onSubscribed = onSubscribed;
+    client?.onSubscribeFail = onSubscribeFail;
+    client?.onUnsubscribed = onUnsubscribed;
+    client?.pongCallback = pong;
+
+    final String username = 'test';
+    final String password = '1234';
+
+    print('Attempting authentication with Username: $username');
+    final connMessage = MqttConnectMessage()
+        .authenticateAs(username, password)
+        .withWillTopic('willTopic/$identifier')
+        .withWillMessage('Client $identifier Disconnected Unexpectedly')
+        .startClean() // Non persistent session for testing
+        .withWillQos(MqttQos.atLeastOnce);
+
+    client?.connectionMessage = connMessage;
+  }
   Future<void> connect() async {
+    if (client == null) {
+      print('MQTT Client not initialized.');
+      return;
+    }
+
+    if (client?.connectionStatus?.state == MqttConnectionState.connected) {
+      print('Already connected.');
+      return;
+    }
     try {
-      print('Connecting');
+      print('MQTT Client connecting...');
       await client?.connect();
-    } catch (e) {
-      print('Exception: $e');
+    } on NoConnectionException catch (e) {
+      print('MQTT Client exception - No connection: $e');
+      client?.disconnect();
+    } on Exception catch (e) {
+      print('MQTT Client exception: $e');
       client?.disconnect();
     }
-    print("connected");
+    if (client?.connectionStatus?.state != MqttConnectionState.connected) {
+      print('Connection failed.');
+    }
+    client?.published!.listen((MqttPublishMessage message) {
+      final payload = MqttPublishPayload.bytesToStringAsString(message.payload.message);
+      print('Published message: payload $payload is published to ${message.variableHeader!.topicName} with Qos ${message.header!.qos}');
+    });
   }
-  void disconnect() {
+
+//connected callback
+  void onConnected() {
+    print('Successfully connected to mqtt broker');
+  }
+  //disconnected callback
+  void onDisconnected() {
     print('Disconnecting MQTT client');
-    client?.disconnect();
   }
+
+  //subscribed callback
+  void onSubscribed(String topic) {
+    print('Subscribed topic: $topic');
+  }
+
+// Subscribed failed callback
+  void onSubscribeFail(String topic) {
+    print('Failed to subscribe $topic');
+  }
+
+// Unsubscribed callback
+  void onUnsubscribed(String? topic) {
+    print('Unsubscribed topic: $topic');
+  }
+
+// Ping callback
+  void pong() {
+    print('Ping response client callback invoked');
+  }
+
 }
